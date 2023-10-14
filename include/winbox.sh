42687d37394d1b4cc043825f5b0b3165d2ed02a8
The_Think3r <thethink3r@gblan.de>
2023-10-14 21:58:02 +0200

    Change Installer to 3.40

--- include/winbox.sh
+++ include/winbox.sh
@@ -3,7 +3,7 @@ export WINEDLLPATH=/app/dlls/lib32:/app/dlls/lib
 export WINEPREFIX=/var/data/wine
 export WINE="/app/bin/wine"
 export WINE_C_DRIVE="${WINEPREFIX}/drive_c"
-export WINBOX_DOWNLOAD_URL='https://download.mikrotik.com/winbox/3.38/winbox64.exe'
+export WINBOX_DOWNLOAD_URL='https://download.mikrotik.com/winbox/3.40/winbox64.exe'
 export WINBOX_RUN_CMD="${WINE_C_DRIVE}/winbox.exe"
 
 if [ ! -f "${WINBOX_RUN_CMD}" ]; then
